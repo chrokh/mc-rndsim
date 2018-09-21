d8200242bc3b0998d80fd1efd56f469b4412a34a
chrokh <christopher.okhravi@gmail.com>
2018-09-21 11:40:27 +0200

    Remove example R scripts that are still too messy. Source all remaining example R scripts in the all.R script.

--- example/stats/all.R
+++ example/stats/all.R
@@ -1,14 +1,5 @@
 pdf('plots/summary.pdf', 9, 8)
 
-print('stats/enpv-01.R')
-source('stats/enpv-01.R')
-
-print('stats/enpv-02.R')
-source('stats/enpv-02.R')
-
-print('stats/enpv.R')
-source('stats/enpv.R')
-
 print('stats/cashflow.R')
 source('stats/cashflow.R')
 
@@ -18,11 +9,13 @@ source('stats/cumulative-cashflow-01.R')
 print('stats/cumulative-cashflow-02.R')
 source('stats/cumulative-cashflow-02.R')
 
-print('stats/cashflow.R')
-source('stats/cashflow.R')
+print('stats/predict-enpv.R')
+source('stats/predict-enpv.R')
+
+print('stats/enpv-per-phase.R')
+source('stats/enpv-per-phase.R')
 
 print('stats/notogo.R')
 source('stats/notogo.R')
-
 
 dev.off()
