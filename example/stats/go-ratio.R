eca225b1603846854963a59a50b285f0a9b6e557
chrokh <christopher.okhravi@gmail.com>
2018-09-26 11:32:26 +0200

    Add some per-market-entry output to go-ratio.R script and do some slight cleanup.

--- example/stats/go-ratio.R
+++ example/stats/go-ratio.R
@@ -12,13 +12,19 @@ tmp$cost     <- tmp$cost0+tmp$cost1+tmp$cost2+tmp$cost3+tmp$cost4+tmp$cost5+tmp$
 tmp$cashflow <- tmp$revenue - tmp$cost
 
 # Compute expected (probabilistic) cashflows
-tmp$ex_cashflow0 <- (tmp$revenue0 - tmp$cost0)
-tmp$ex_cashflow1 <- (tmp$revenue1 - tmp$cost1) * tmp$prob0
-tmp$ex_cashflow2 <- (tmp$revenue2 - tmp$cost2) * tmp$prob0 * tmp$prob1
-tmp$ex_cashflow3 <- (tmp$revenue3 - tmp$cost3) * tmp$prob0 * tmp$prob1 * tmp$prob2
-tmp$ex_cashflow4 <- (tmp$revenue4 - tmp$cost4) * tmp$prob0 * tmp$prob1 * tmp$prob2 * tmp$prob3
-tmp$ex_cashflow5 <- (tmp$revenue5 - tmp$cost5) * tmp$prob0 * tmp$prob1 * tmp$prob2 * tmp$prob3 * tmp$prob4
-tmp$ex_cashflow  <- tmp$ex_cashflow0+tmp$ex_cashflow1+tmp$ex_cashflow2+tmp$ex_cashflow3+tmp$ex_cashflow4+tmp$ex_cashflow5
+tmp$EXcashflow0 <- (tmp$revenue0 - tmp$cost0)
+tmp$EXcashflow1 <- (tmp$revenue1 - tmp$cost1) * tmp$prob0
+tmp$EXcashflow2 <- (tmp$revenue2 - tmp$cost2) * tmp$prob0 * tmp$prob1
+tmp$EXcashflow3 <- (tmp$revenue3 - tmp$cost3) * tmp$prob0 * tmp$prob1 * tmp$prob2
+tmp$EXcashflow4 <- (tmp$revenue4 - tmp$cost4) * tmp$prob0 * tmp$prob1 * tmp$prob2 * tmp$prob3
+tmp$EXcashflow5 <- (tmp$revenue5 - tmp$cost5) * tmp$prob0 * tmp$prob1 * tmp$prob2 * tmp$prob3 * tmp$prob4
+tmp$EXcashflow  <- tmp$EXcashflow0+tmp$EXcashflow1+tmp$EXcashflow2+tmp$EXcashflow3+tmp$EXcashflow4+tmp$EXcashflow5
+
+# Sum up PoS
+tmp$prob <- tmp$prob0+tmp$prob1+tmp$prob2+tmp$prob3+tmp$prob4+tmp$prob5
+
+# Compute expected market count
+tmp$EXmarket_count <- ifelse(tmp$conseq_decision5 == 'true', tmp$prob, 0)
 
 # Create base subset and rename columns to prepare for merge
 base <- subset(tmp, tmp$group == 'base')
@@ -29,20 +35,30 @@ merged <- merge(tmp, base, by.x='id', by.y='xid')
 
 # Compute promised and expected intervention spends
 merged$spend <- merged$cashflow - merged$xcashflow
-merged$ex_spend <- merged$ex_cashflow - merged$xex_cashflow
+merged$EXspend <- merged$EXcashflow - merged$xEXcashflow
 
 # Count the number of go-decisions and the interventions' price-efficiencies in terms of go-decisions
 counted <- ddply(merged, .(group), summarize,
-                 xgos        = length(xconseq_decision5[xconseq_decision5 == 'true']),
-                 gos         = length(conseq_decision5[conseq_decision5 == 'true']),
-                 n           = length(id),
-                 gorate      = gos / n,
-                 xgorate     = xgos / n,
-                 goratediv   = gorate / xgorate,
-                 spend       = sum(spend),
-                 ex_spend    = sum(ex_spend),
-                 goratediv_per_spend = goratediv / spend,
-                 goratediv_per_ex_spend = goratediv / ex_spend
+                 
+                 # totals
+                 xgos         = length(xconseq_decision5[xconseq_decision5 == 'true']),
+                 gos          = length(conseq_decision5[conseq_decision5 == 'true']),
+                 n            = length(id),
+                 m            = sum(EXmarket_count),
+                 tot_spend    = sum(spend),
+                 tot_EXspend  = sum(EXspend),
+                 
+                 # per entry
+                 gorate           = gos / n,
+                 xgorate          = xgos / n,
+                 avg_spend        = tot_spend / n,
+                 avg_EXspend      = tot_EXspend / n,
+                 gorateimpr       = gorate / xgorate,
+                 spend_per_impr   = tot_spend / gorateimpr,
+                 EXspend_p_impr   = tot_EXspend / gorateimpr,
+                 
+                 # per exits
+                 spend_per_market   = tot_spend / m,
+                 EXspend_per_market = tot_EXspend / m
 )
-
 counted
